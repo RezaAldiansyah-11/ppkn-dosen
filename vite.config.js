a8d6f3edd657ee1fe324b8c2ac4c699ab7dae6fd
octavium2000 <rezewithansecond@gmail.com>
2026-07-11 22:08:12 +0700

    bug

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const isVercel = process.env.VERCEL === "true";
 export default defineConfig({
   plugins: [react()],
-  base: isVercel ? "/" : "/ppkn-dosen/",
 });
